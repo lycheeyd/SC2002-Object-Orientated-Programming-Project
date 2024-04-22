687c5a190fb8a9e5c8d170ff51288e2aaba33518
lycheeyd <165633195+lycheeyd@users.noreply.github.com>
2024-04-22 17:49:11 +0800

    Add files via upload

--- com.branch/Branch.java
+++ com.branch/Branch.java
@@ -1,9 +1,12 @@
 package com.branch;
 
+import com.cache.EmployeeCache;
+
 public class Branch {
     private BranchName branchName;
     private String location;
     private int staffQuota;
+    private boolean branchStatus;
 /*    private List<Staff> staffMembers;
     private List<MenuItem> menu;
     private List<Order> orders; */
@@ -37,6 +40,14 @@ public class Branch {
     }
 */
 
+    public boolean getBranchStatus(){
+        return this.branchStatus;
+    }
+
+    public void setBranchStatus(boolean stat){
+        this.branchStatus = stat;
+    }
+
     public BranchName getBranchName() {
         return this.branchName;
     }
@@ -59,6 +70,43 @@ public class Branch {
 
     public void setStaffQuota(int staffQuota) {
         this.staffQuota = staffQuota;
+    }
+
+    //return no. of staff + manager, logic done in UI
+    public int getStaffCount(){
+        return cache.getFilteredItems(Branch.getBranchName(), StaffRole.S).size();
+    }
+
+    public int getManagerCount(){
+        return cache.getFilteredItems(Branch.getBranchName(), StaffRole.M).size();
+    }
+
+    public static boolean quotaFull(Branch branch, EmployeeCache cache){
+        int staffCount = cache.getFilteredItems(Branch.getBranchName(), StaffRole.S).size();
+        int managerCount = cache.getFilteredItems(Branch.getBranchName(), StaffRole.M).size();
+        if (staffCount + managerCount == quota){
+            System.out.println("error: staff quota has been reached");
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean managerFull(Branch branch, EmployeeCache cache){
+        int staffCount = cache.getFilteredItems(Branch.getBranchName(), StaffRole.S).size();
+        int managerCount = cache.getFilteredItems(Branch.getBranchName(), StaffRole.M).size();
+        if (staffCount <= 4 && managerCount == 1) {
+            System.out.println("error: manager quota has been reached");
+            return true;
+        }
+        else if (staffCount <=8 && managerCount == 2){
+            System.out.println("error: manager quota has been reached");
+            return true;
+        }
+        else if (staffCount <= 15 && managerCount == 3){
+            System.out.println("error: manager quota has been reached");
+            return true;
+        }
+        return false;
     }
 
     @Override
--- com.branch/BranchName.java
+++ com.branch/BranchName.java
@@ -1,5 +1,23 @@
 package com.branch;
 
-public enum BranchName {
-    NTU, JP, JE;
+import java.util.function.Predicate;
+
+import com.menu.MenuItem;
+
+public enum BranchName implements Predicate<Object> {
+    NTU,
+    JP, 
+    JE;
+
+    @Override
+    public boolean test(Object o) {
+        if (o instanceof Branch) {
+            return ((Branch) o).getBranchName().equals(this);
+        }
+        else if (o instanceof MenuItem) {
+            return ((MenuItem) o).getBranch().getBranchName().equals(this);
+        }
+        return false;
+    }
+
 }
